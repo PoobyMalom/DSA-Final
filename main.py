e6e5b9ab2b8864aff89fa9ccec0392b39e346169
coxlaurel <coxlaurel@gmail.com>
2025-04-17 10:38:22 -0400

    divide input_arr into blocks

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello")
+def block_sort(input_arr, block_size):
+    blocks = [] # nested lists to store blocks
+
+    # Break input_arr into blocks
+    temp_block = []
+    for num in input_arr:
+        if len(temp_block) == block_size:
+            temp_block.sort()
+            blocks.append(temp_block)
+            temp_block = []
+        temp_block.append(num)
+    temp_block.sort()
+    blocks.append(temp_block)
+
+
+test_arr = [3,5,6,3,2,1,5,6]
+block_sort(test_arr, 3)
