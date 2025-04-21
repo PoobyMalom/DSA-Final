e6a90147d35c0d3f60b2000594de9b20e0c12a98
Toby Mallon <57491618+PoobyMalom@users.noreply.github.com>
2025-04-21 15:43:37 -0400

    Finish Implementation

--- helpers.py
+++ helpers.py
@@ -1,28 +1,12 @@
-def floorpoweroftwo(x):
-    x = x | (x >> 1)
-    x = x | (x >> 2)
-    x = x | (x >> 4)
-    x = x | (x >> 8)
-    x = x | (x >> 16)
-    x = x | (x >> 32)
-    return x - (x >> 1)
-
-
-def reverse(array, range):
-    temp = array[range.start:range.stop+1]
-    temp.reverse()
-    array[range.start:range.stop+1] = temp
-
-
-def rotate(array, ammount, r):
-    reverse(array, r)
-    reverse(array, range(r.start, r.start + ammount))
-    reverse(array, range(r.start + ammount, r.stop))
-
-
-def swap(array, index1, index2):
-    array[index1], array[index2] = array[index2], array[index1]
-
-
-def block_swap(array, A, B):
-    rotate(array)
+def insertionSort(nums):
+	# Traverse through 1 to len(nums)
+    for i in range(1, len(nums)):
+        j = i - 1
+        # As long as we are in-bounds
+        while j >= 0 and nums[j + 1] < nums[j]:
+            # nums[j] and nums[j + 1] are out of order so swap them 
+            tmp = nums[j + 1]
+            nums[j + 1] = nums[j]
+            nums[j] = tmp
+            j -= 1
+    return nums
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from helpers import insertionSort
+import random
+
 def block_sort(input_arr, block_size):
     blocks = [] # nested lists to store blocks
 
@@ -5,13 +8,31 @@ def block_sort(input_arr, block_size):
     temp_block = []
     for num in input_arr:
         if len(temp_block) == block_size:
-            temp_block.sort()
+            temp_block = insertionSort(temp_block)
             blocks.append(temp_block)
             temp_block = []
         temp_block.append(num)
     temp_block.sort()
     blocks.append(temp_block)
 
+    result = []
 
-test_arr = [3,5,6,3,2,1,5,6]
-block_sort(test_arr, 3)
+    while blocks:
+        min_index = 0
+        for i in range(1, len(blocks)):
+            if blocks[i][0] < blocks[min_index][0]:
+                min_index = i
+
+        result.append(blocks[min_index].pop(0))
+
+        if len(blocks[min_index]) == 0:
+            blocks.pop(min_index)
+
+    return result
+        
+
+
+test_arr = list(range(1, 100000))
+random.shuffle(test_arr)
+r = block_sort(test_arr, 1000)
+print(r)
